cec3b9683074d31c280db933d57a66077169aba4
William Alvarez <xwillalvarezx@gmail.com>
2023-12-22 13:59:31 -0500

    Can now correctly fetch all sets into set table

--- initialize_cards.sql
+++ initialize_cards.sql
@@ -43,3 +43,21 @@ CREATE TABLE IF NOT EXISTS cards(
 ,   set_name            TEXT
 ,   id                  TEXT
 );
+
+DROP TABLE IF EXISTS sets CASCADE;
+
+CREATE TABLE IF NOT EXISTS sets(
+    code                    TEXT
+,   "name"                  TEXT
+,   gatherer_code           TEXT
+,   old_code                TEXT
+,   magic_cards_info_code   TEXT
+,   release_date            TEXT
+,   border                  TEXT
+,   "type"                  TEXT
+,   block                   TEXT
+,   online_only             TEXT
+,   booster                 TEXT
+,   mkm_id                  TEXT
+,   mkm_name                TEXT
+);
--- initializer.py
+++ initializer.py
@@ -11,45 +11,33 @@ connect = psycopg2.connect(
     )
 cursor = connect.cursor()
 
-initialize_sql = open(r"C:\Users\xwill\OneDrive\Desktop\Scryfall2.0\initialize.sql",'r')
-
 # Initialize the Cards table in the database
 def initialize_cards():
     cardList = Card.where(cmc='15')\
                         .all()
-    
+    initialize_sql = open(r"C:\Users\xwill\OneDrive\Desktop\Scryfall2.0\initialize_cards.sql",'r')
     cursor.execute(initialize_sql.read())
     
     insertCard = "INSERT INTO cards (name, multiverse_id, layout, names, mana_cost, cmc, colors, color_identity, type, supertypes, subtypes, rarity, text, flavor, artist, number, power, toughness, loyalty, variations, watermark, border, timeshifted, hand, life, release_date, starter, rulings, foreign_names, printings, original_text, original_type, legalities, source, image_url, set, set_name, id) VALUES ($${0}$$, $${1}$$, $${2}$$, $${3}$$, $${4}$$, $${5}$$, $${6}$$, $${7}$$, $${8}$$, $${9}$$, $${10}$$, $${11}$$, $${12}$$, $${13}$$, $${14}$$, $${15}$$, $${16}$$, $${17}$$, $${18}$$, $${19}$$, $${20}$$, $${21}$$, $${22}$$, $${23}$$, $${24}$$, $${25}$$, $${26}$$, $${27}$$, $${28}$$, $${29}$$, $${30}$$, $${31}$$, $${32}$$, $${33}$$, $${34}$$, $${35}$$, $${36}$$, $${37}$$);"
     for x in cardList:
         cursor.execute(insertCard.format(x.name, x.multiverse_id, x.layout, x.names, str(x.mana_cost),  x.cmc, x.colors, x.color_identity, x.type, x.supertypes, x.subtypes, x.rarity, x.text, x.flavor, x.artist, x.number, x.power, x.toughness, x.loyalty, x.variations, x.watermark, x.border, x.timeshifted, x.hand, x.life, x.release_date, x.starter, x.rulings, x.foreign_names, x.printings, x.original_text, x.original_type, x.legalities, x.source, x.image_url, x.set, x.set_name, x.id))
     
-    cursor.execute("SELECT name from cards order by name;")
-    result = cursor.fetchall()
-    print("Result Set: \n")
-    for x in result:
-        print(x)
-    
     cursor.connection.commit()
 
 def initialize_sets():
-    setList = sets = Set.where(name='khans').all()
+    setList = sets = Set.all()
     
+    initialize_sql = open(r"C:\Users\xwill\OneDrive\Desktop\Scryfall2.0\initialize_sets.sql",'r')
     cursor.execute(initialize_sql.read())
     
-    insertCard = "INSERT INTO cards (code, name, gatherer_code, old_code, magic_cards_info_code, release_date, border, type, block, online_only, booster, mkm_id, mkm_name) VALUES ($${0}$$, $${1}$$, $${2}$$, $${3}$$, $${4}$$, $${5}$$, $${6}$$, $${7}$$, $${8}$$, $${9}$$, $${10}$$, $${11}$$, $${12}$$, $${13}$$);"
-    for x in cardList:
-        cursor.execute(insertCard.format(x.name, x.multiverse_id, x.layout, x.names, str(x.mana_cost),  x.cmc, x.colors, x.color_identity, x.type, x.supertypes, x.subtypes, x.rarity, x.text, x.flavor, x.artist, x.number, x.power, x.toughness, x.loyalty, x.variations, x.watermark, x.border, x.timeshifted, x.hand, x.life, x.release_date, x.starter, x.rulings, x.foreign_names, x.printings, x.original_text, x.original_type, x.legalities, x.source, x.image_url, x.set, x.set_name, x.id))
-    
-    cursor.execute("SELECT name from cards order by name;")
-    result = cursor.fetchall()
-    print("Result Set: \n")
-    for x in result:
-        print(x)
+    insertSets = "INSERT INTO sets (code, name, gatherer_code, old_code, magic_cards_info_code, release_date, border, type, block, online_only, booster, mkm_id, mkm_name) VALUES ($${0}$$, $${1}$$, $${2}$$, $${3}$$, $${4}$$, $${5}$$, $${6}$$, $${7}$$, $${8}$$, $${9}$$, $${10}$$, $${11}$$, $${12}$$);"
+    for x in setList:
+        cursor.execute(insertSets.format(x.code, x.name, x.gatherer_code, x.old_code, x.magic_cards_info_code,  x.release_date, x.border, x.type, x.block, x.online_only, x.booster, x.mkm_id, x.mkm_name))
     
     cursor.connection.commit()
 
 
-initialize_cards()
+#initialize_cards()
+initialize_sets()
 
 cursor.close()
