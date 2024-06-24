5f8cace54e8e4555c06d5dba1df794d412e05d76
William Alvarez <xwillalvarezx@gmail.com>
2024-06-24 14:29:43 -0400

    small update

--- initialize_cardsHS.sql
+++ initialize_cardsHS.sql
@@ -7,14 +7,14 @@ DROP TABLE IF EXISTS cards_HS CASCADE;
 
 CREATE TABLE IF NOT EXISTS cards_HS(
     id                           INTEGER PRIMARY KEY
-,   collectible                  INTEGER
+,   collectible                  INTEGER --need to pull cards with collectable = 0
 ,   slug                         TEXT
-,   classId                      INTEGER
+,   classId                      INTEGER --need to create new table which maps class id to a class name. multiclass cards seem to take the first class id in their list of classes. triclass cards seem to take the nuetral class id
 --,   multiClassIds                TEXT   --list
-,   spellSchoolId                INTEGER
-,   cardTypeId                   INTEGER
-,   cardSetId                    INTEGER
-,   rarityId                     INTEGER  
+,   spellSchoolId                INTEGER --need to map id to spellschool name
+,   cardTypeId                   INTEGER -- need to map card type id to card type name
+,   cardSetId                    INTEGER --needs mapping
+,   rarityId                     INTEGER --needs mapping
 ,   artistName                   TEXT
 ,   manaCost                     INTEGER
 ,   "name"                       TEXT
@@ -26,82 +26,74 @@ CREATE TABLE IF NOT EXISTS cards_HS(
 --,   keywordIds                   TEXT   --list
 ,   isZilliaxFunctionalModule    BOOLEAN
 ,   isZilliaxCosmeticModule      BOOLEAN
-,   copyOfCardId                 INTEGER
+,   copyOfCardId                 INTEGER --this data seems to be a remap to the card that it was copied from
 ,   health                       INTEGER
 ,   attack                       INTEGER
-,   minionTypeId                 INTEGER
+,   minionTypeId                 INTEGER --needs a map to minion type name
 --,   childIds                     TEXT   --list
 ,   armor                        INTEGER
 --,   multiTypeIds                 TEXT   --list
 ,   "durability"                 INTEGER
 --,   runeCost                     TEXT   --dictionary
-,   parentId                     INTEGER
-,   bannedFromSideboard          INTEGER --should be boolean?
+,   parentId                     INTEGER --this seems to be the id of the card who is the actual parent of the character in the card. IE magni bronzebeards parent id is 7 and his parent is gromash? I believe most of the parent IDs refrence uncollectible cards
+,   bannedFromSideboard          INTEGER --should be boolean? seems like all banned from sideboard cards are 1 and everything else is null
 ,   maxSideboardCards            INTEGER
-
-);
-
-DROP TABLE IF EXISTS audio_HS CASCADE;
-
-CREATE TABLE IF NOT EXISTS audio_HS(
-    audio_id           INT PRIMARY KEY
-,   id                 INT
-,   audio_type         TEXT
-,   audio              TEXT
-,   FOREIGN KEY(id)
-        REFERENCES cards_HS(id)
 );
 
 
 DROP TABLE IF EXISTS classes_HS CASCADE;
 
 CREATE TABLE IF NOT EXISTS classes_HS(
-    class_id           INT PRIMARY KEY
-,   class_name         TEXT
+    class_id                    INTEGER PRIMARY KEY
+,   class_name                  TEXT
+,   slug                        TEXT
+,   hero_card_id                INTEGER
+,   hero_power_card_id          INTEGER
+,   alternative_hero_card_ids   INTEGER -- list this needs to be its own table
 );
 
 DROP TABLE IF EXISTS classes_link_HS CASCADE;
 
 CREATE TABLE IF NOT EXISTS classes_link_HS(
-    id                 INT
-,   class_id           INT
-,   FOREIGN KEY(id)
+    card_id            INTEGER
+,   class_id           INTEGER 
+,   FOREIGN KEY(card_id)
         REFERENCES cards_HS(id)
 ,   FOREIGN KEY(class_id)
         REFERENCES classes_HS(class_id)
 );
 
-DROP TABLE IF EXISTS mechanics_HS CASCADE;
+DROP TABLE IF EXISTS keywords_HS CASCADE;
 
-CREATE TABLE IF NOT EXISTS mechanics_HS(
-    mechanic_id        TEXT PRIMARY KEY
-,   mechanic_name      TEXT
+CREATE TABLE IF NOT EXISTS keywords_HS(
+    keyword_id        TEXT PRIMARY KEY
+,   keyword_name      TEXT
 );
 
-DROP TABLE IF EXISTS mechanics_link_HS CASCADE;
+DROP TABLE IF EXISTS keywords_link_HS CASCADE;
 
-CREATE TABLE IF NOT EXISTS mechanics_link_HS(
-    id                 INT
-,   mechanic_id        TEXT
-,   FOREIGN KEY(id)
+CREATE TABLE IF NOT EXISTS keywords_link_HS(
+    card_id                 INTEGER 
+,   keyword_id              TEXT
+,   FOREIGN KEY(card_id)
         REFERENCES cards_HS(id)
-,   FOREIGN KEY(mechanic_id)
-        REFERENCES mechanics_HS(mechanic_id)
+,   FOREIGN KEY(keyword_id)
+        REFERENCES keywords_HS(keyword_id)
 );
 
 DROP TABLE IF EXISTS referenced_tags_HS CASCADE;
 
 CREATE TABLE IF NOT EXISTS referenced_tags_HS(
-    tag_id             INT PRIMARY KEY
+    tag_id             INTEGER PRIMARY KEY
 ,   tag_name           TEXT
 );
 
 DROP TABLE IF EXISTS referenced_tags_link_HS CASCADE;
 
 CREATE TABLE IF NOT EXISTS referenced_tags_link_HS(
-    id                 INT
-,   tag_id             INT
-,   FOREIGN KEY(id)
+    card_id                 INTEGER
+,   tag_id             INTEGER
+,   FOREIGN KEY(card_id)
         REFERENCES cards_HS(id)
 ,   FOREIGN KEY(tag_id)
         REFERENCES referenced_tags_HS(tag_id)
@@ -110,8 +102,8 @@ CREATE TABLE IF NOT EXISTS referenced_tags_link_HS(
 DROP TABLE IF EXISTS counterpart_cards_HS CASCADE;
 
 CREATE TABLE IF NOT EXISTS counterpart_cards_HS(
-    id_main           INT
-,   id_counterpart    INT
+    id_main           INTEGER
+,   id_counterpart    INTEGER
 ,   FOREIGN KEY(id_main)
         REFERENCES cards_HS(id)
 ,   FOREIGN KEY(id_counterpart)
@@ -128,9 +120,9 @@ CREATE TABLE IF NOT EXISTS races_HS(
 DROP TABLE IF EXISTS races_link_HS CASCADE;
 
 CREATE TABLE IF NOT EXISTS races_link_HS(
-    id                 INT
+    card_id             INTEGER
 ,   race_id            TEXT
-,   FOREIGN KEY(id)
+,   FOREIGN KEY(card_id)
         REFERENCES cards_HS(id)
 ,   FOREIGN KEY(race_id)
         REFERENCES races_HS(race_id)
@@ -139,16 +131,16 @@ CREATE TABLE IF NOT EXISTS races_link_HS(
 DROP TABLE IF EXISTS spellschools_HS CASCADE;
 
 CREATE TABLE IF NOT EXISTS spellschools_HS(
-    spellschool_id     INT PRIMARY KEY
+    spellschool_id     INTEGER PRIMARY KEY
 ,   spellschool_name   TEXT
 );
 
 DROP TABLE IF EXISTS spellschools_link_HS CASCADE;
 
 CREATE TABLE IF NOT EXISTS spellschools_link_HS(
-    id                 INT
-,   spellschool_id     INT
-,   FOREIGN KEY(id)
+    card_id                 INTEGER
+,   spellschool_id     INTEGER
+,   FOREIGN KEY(card_id)
         REFERENCES cards_HS(id)
 ,   FOREIGN KEY(spellschool_id)
         REFERENCES spellschools_HS(spellschool_id)
--- initializer.py
+++ initializer.py
@@ -41,6 +41,24 @@ insertCard = """
         %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
     );
 """
+insertClasses = """
+    INSERT INTO classes_HS (
+        class_id
+,       class_name
+
+    ) VALUES (
+        %s, %s
+    );
+"""
+insertClassesLink = """
+    INSERT INTO classes_link_HS (
+        card_id
+,       class_id
+
+    ) VALUES (
+        %s, %s
+    );
+"""
 
 insertRace = """
     INSERT INTO races_HS (
@@ -60,32 +78,31 @@ insertRaceLink = """
         %s, %s
     );
 """
-insertMechanic = """
-    INSERT INTO mechanics_HS (
-        mechanic_id
-,       mechanic_name
+insertKeyword = """
+    INSERT INTO keywords_HS (
+        keyword_id
+,       keyword_name
 
     ) VALUES (
         %s, %s
     );
 """
-insertMechanicLink = """
-    INSERT INTO mechanics_link_HS (
+insertKeywordLink = """
+    INSERT INTO keywords_link_HS (
         card_id
-,       mechanic_id
+,       keyword_id
 
     ) VALUES (
         %s, %s
     );
 """
 
-def get_posts(**kwargs):
-    # this string needs to be created by adding all the criteria needed plus the token that is generated in OAuthToken.js
-    
-    url = 'https://us.api.blizzard.com/hearthstone/cards?locale=en_US&access_token=USySTrAtR6Ads4rR9qRY4FTF7TGsEQJs4z'
+def get_posts_cards(**kwargs):
+    # this string needs to be created by adding all the criteria needed plus the token that is generated in OAuthToken.js    
+    url = 'https://us.api.blizzard.com/hearthstone/cards?locale=en_US&access_token=UST8Yl2W7341B0zq9Zx5Tkgims2QcrjjKL'
     for key, value in kwargs.items():
         url = url + "&%s=%s" % (key, value)
-        print(url)
+    print(url)
     #send request with request url and request method with the payload
     try:
         response = requests.get(url)
@@ -100,6 +117,81 @@ def get_posts(**kwargs):
         print('Error:', e)
         return None
 
+def get_posts_meta_data(**kwargs):
+    url = 'https://us.api.blizzard.com/hearthstone/metadata?locale=en_US&access_token=USySTrAtR6Ads4rR9qRY4FTF7TGsEQJs4z'
+    for key, value in kwargs.items():
+        url = url + "&%s=%s" % (key, value)
+    print(url)
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            posts = response.json()
+            return posts
+        else:
+            print('Error:', response.status_code)
+            return None
+    except requests.exceptions.RequestException as e:
+        print('Error:', e)
+        return None
+
+def intitalize_cards(cursor, currPage):
+    curPageCardsList = currPage.get('cards') # get post returns 4 dictionaries: cards, cardCount, pageCount, and page
+    for card in curPageCardsList:
+        cursor.execute(insertCard, ( #inserst all card information into card table
+            card.get('id')
+        ,   card.get('collectible')
+        ,   card.get('slug')
+        ,   card.get('classId')
+        #,   card.get('multiClassIds') -> needs own table
+        ,   card.get('spellSchoolId')
+        ,   card.get('cardTypeId')
+        ,   card.get('cardSetId')
+        ,   card.get('rarityId')
+        ,   card.get('artistName')
+        ,   card.get('manaCost')
+        ,   card.get('name')
+        ,   card.get('text')
+        ,   card.get('image')
+        ,   card.get('imageGold')
+        ,   card.get('flavorText')
+        ,   card.get('cropImage')
+        #,   card.get('keywordIds') -> needs own table
+        ,   card.get('isZilliaxFunctionalModule')
+        ,   card.get('isZilliaxCosmeticModule')
+        ,   card.get('copyOfCardId')   
+        ,   card.get('health')
+        ,   card.get('attack')
+        ,   card.get('minionTypeId')
+        #,   card.get('childIds') -> needs own table
+        ,   card.get('armor')
+        #,   card.get('multiTypeIds') -> needs own table
+        ,   card.get('durability')
+        #,   card.get('runeCost') dictionary -> needs own table
+        ,   card.get('parentId')
+        ,   card.get('bannedFromSideboard')
+        ,   card.get('maxSideboardCards')
+        ))
+        
+        # #inserts class info into classes link tables
+        # multiClassIds = card.get('multiClassIds')
+        # if multiClassIds:
+        #     for classId in multiClassIds:
+        #         cursor.execute(insertClassesLink,(
+        #             card.get('id')
+        #         ,   classId
+        #         ))
+        
+        # #inserts keyword info into keywords_link tables
+        # keywordIds = card.get('keywordIds') 
+        # if keywordIds:
+        #     for keywordId in keywordIds:
+        #         cursor.execute(insertKeywordLink,(
+        #             card.get('id')
+        #         ,   keywordId
+        #         ))
+
+
+
 
 def main():
 
@@ -113,59 +205,35 @@ def main():
     cursor = connect.cursor()
 
     # Initialize the HS card Table
-    initialize_sql_HS = open(r"C:\Users\xwill\OneDrive\Desktop\Scryfall2.0\initialize_cardsHS.sql",'r')
+    initialize_sql_HS = open(r"C:\Users\xwill\OneDrive\Desktop\projects\Scryfall2.0\initialize_cardsHS.sql",'r')
     cursor.execute(initialize_sql_HS.read())
 
-    # this is commented out until i can close the server when I finish
+    pageCount = get_posts_cards(collectible='0,1').get('pageCount')
+    if pageCount:
+        for page in range(1, pageCount + 1): #loop over this for each page
+            currPage = get_posts_cards(colletible='0,1',page=str(page))
+            intitalize_cards(cursor, currPage)
+    else:
+        print('Failed to fetch from API')
+    print('all cards initialized')
 
-    pageCount = get_posts().get('pageCount')
-    allKeys = {}
-    for page in range(1, pageCount + 1): #loop over this for each page
-        cards = get_posts(page = str(page))
-        curPageCardsList = cards.get('cards') # get post returns 4 dictionaries: cards, cardCount, pageCount, and page
-        if cards:
-            for card in curPageCardsList:
-                # keyList = list(card.keys())
-                # for key in keyList:
-                #     if key not in allKeys:
-                #         allKeys[key] = type(card.get(key))
-                cursor.execute(insertCard, (
-                    card.get('id')
-                ,   card.get('collectible')
-                ,   card.get('slug')
-                ,   card.get('classId')
-                #,   card.get('multiClassIds') -> needs own table
-                ,   card.get('spellSchoolId')
-                ,   card.get('cardTypeId')
-                ,   card.get('cardSetId')
-                ,   card.get('rarityId')
-                ,   card.get('artistName')
-                ,   card.get('manaCost')
-                ,   card.get('name')
-                ,   card.get('text')
-                ,   card.get('image')
-                ,   card.get('imageGold')
-                ,   card.get('flavorText')
-                ,   card.get('cropImage')
-                #,   card.get('keywordIds') -> needs own table
-                ,   card.get('isZilliaxFunctionalModule')
-                ,   card.get('isZilliaxCosmeticModule')
-                ,   card.get('copyOfCardId')   
-                ,   card.get('health')
-                ,   card.get('attack')
-                ,   card.get('minionTypeId')
-                #,   card.get('childIds') -> needs own table
-                ,   card.get('armor')
-                #,   card.get('multiTypeIds') -> needs own table
-                ,   card.get('durability')
-                #,   card.get('runeCost') dictionary -> needs own table
-                ,   card.get('parentId')
-                ,   card.get('bannedFromSideboard')
-                ,   card.get('maxSideboardCards')
-                ))
-        else:
-            print('failed to fetch posts from api')
-    # print(allKeys)
+
+    # meta_data = get_posts_meta_data()
+    # classes = meta_data.get('classes')
+    # for eachClass in classes:
+    #     cursor.execute(insertClasses,
+    #        eachClass.get()
+    #     ,  eachClass.get()
+    #     ,  eachClass.get()
+    #     ,  eachClass.get()
+    #     ,  eachClass.get()
+    #     )
+    # allMetaKeys = []
+    # for key in meta_data:
+    #     allMetaKeys.append(key)
+    # print(allMetaKeys)
+    #intitalize_cards(cursor)
+    
     cursor.connection.commit()
     cursor.close()
 
